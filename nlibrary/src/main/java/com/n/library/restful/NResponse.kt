2287aa0261e5a860c2b828683d3bffec4b5d529d
牛梦林 <799978248@qq.com>
2022-05-12 16:27:52 +0800

    feat: Response响应报文-状态码新增

--- nlibrary/src/main/java/com/n/library/restful/NResponse.kt
+++ nlibrary/src/main/java/com/n/library/restful/NResponse.kt
@@ -5,7 +5,20 @@ package com.n.library.restful
  */
 open class NResponse<T> {
     companion object {
-        val SUCCESS: Int = 0;
+        val SUCCESS: Int = 0
+        val RC_HAS_ERROR = 5000 //有错误
+        val RC_ACCOUNT_INVALID = 5001 //账号不存在
+        val RC_PWD_INVALID = 5002 //密码错误
+        val RC_NEED_LOGIN = 5003 //请先登录
+        val RC_NOT_PURCHASED = 5004 //未购买本课程，或用户ID有误
+        val RC_CHECK_SERVER_ERROR = 5005 //效验服务报错
+        val RC_USER_NAME_EXISTS = 5006 //此用户名被暂用
+        val RC_HTML_INVALID = 8001 //请输入HTML
+        val RC_CONFIG_INVALID = 8002 //请输入配置
+
+        val RC_USER_FORBID = 6001 //用户身份非法，如有疑问可进入课程官网群联系管理员
+        val RC_AUTH_TOKEN_EXPIRED = 4030 //访问Token过期，请重新设置
+        val RC_AUTH_TOKEN_INVALID = 4031 //访问Token不正确，请重新设置
     }
     var rawData: String? = null //原始数据
     var code = 0 //业务状态码
